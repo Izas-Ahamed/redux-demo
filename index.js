be64d98ea94659c0405855cf1f1afd88022c3a7c
THUNDERX <thuderxcodes@gmail.com>
2023-08-23 01:12:53 +0530

    added logger middleware

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ const { wordsReducer } = require("./reducers/wordsReducer");
 const redux = require("redux");
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+//logger
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
 
+//combining reducers
 const rootReducer = combineReducers({
   count: counterReducer,
   words: wordsReducer,
 });
-const store = createStore(rootReducer);
+
+// adding logger middleware
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("Initial State :", store.getState());
 
 //callback fires whenever state got updated and returns unsubscribe function if you don't want to emit callback after
@@ -26,8 +33,8 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(increamentValue());
 store.dispatch(increamentValue(3));
 store.dispatch(decreamentValue());
-store.dispatch(addWord());
-store.dispatch(addWord("hello folks!"));
 
 //unsubscribing event
 unsubscribe();
+store.dispatch(addWord());
+store.dispatch(addWord("hello folks!"));
