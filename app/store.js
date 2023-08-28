ca45b97cb036cdfc10410d67631bbf3f96eadee2
THUNDERX <thuderxcodes@gmail.com>
2023-08-28 21:23:38 +0530

    added logger middleware

--- app/store.js
+++ app/store.js
@@ -1,12 +1,17 @@
-const { configureStore } = require("@reduxjs/toolkit");
+const { configureStore, getDefaultMiddleware } = require("@reduxjs/toolkit");
 const { counterReducer } = require("../features/counter/counterSlice");
 const { wordsReducer } = require("../features/words/wordsSlice");
-
+const logger = require("redux-logger").default;
 const store = configureStore({
   reducer: {
     count: counterReducer,
     words: wordsReducer,
   },
+  //along with default middlewares we concating our desired middleware
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  //
+  //we can also use like this if we only need listed middlewares than defaults
+  //middleware:[logger]
 });
 
 module.exports = store;
